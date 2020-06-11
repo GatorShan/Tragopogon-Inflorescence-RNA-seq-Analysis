1dc5c9f71a364d07e04e97710b099f3f6ef373ae
Shengchen Shan <shan158538@ufl.edu>
2020-06-11 14:50:18 -0400

    Update ExtractHeader.py

--- Annotation/Trinotate_Functional_Annotation/ExtractHeader.py
+++ Annotation/Trinotate_Functional_Annotation/ExtractHeader.py
@@ -3,14 +3,14 @@
 import sys
 from Bio import SeqIO
 
-InFileName = sys.argv[1]
-OutFileName = sys.argv[2]
+InFileName = sys.argv[1] # the fasta file contains all genes/supertranscripts
+OutFileName = sys.argv[2] # the gene_trans_map file
 
 OutFile = open(OutFileName, 'w')
 
 with open(InFileName, "rU") as InFile:
 	for record in SeqIO.parse(InFile, "fasta"):
-		Header = str(record.id) + "\t" + str(record.id)
-		OutFile.write(Header+"\n")
+		Header = str(record.id) + "\t" + str(record.id) # duplicate the header line of the fasta file, separated by a tab
+		OutFile.write(Header+"\n") # write out the duplicated header line
 
 OutFile.close()
